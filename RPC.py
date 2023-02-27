b1d70cb29ac409cce0c9b08c026ebc94a12e20ac
Hasan Syed <syed_hasan@bah.com>
2023-02-27 16:47:34 -0500

    sends image data now

--- RPC.py
+++ RPC.py
@@ -9,19 +9,33 @@ channel = connection.channel()
 
 channel.queue_declare(queue='rpc_queue')
 
+class Image(object):
+    __slots__ = ["filename"]
+    def __init__(self, filename):
+        self.filename = filename
+
+    @property
+    def get(self):
+        with open(self.filename, "rb") as f:
+            data = f.read()
+        return data
+
 def on_request(ch, method, props, body):
     randomNumber = random.randint(0,100)
     dir_list = os.listdir('images')
     index = randomNumber % len(dir_list)
     file_name = dir_list[index]
     print(" [.] file_name: " + file_name)
-    response = file_name
+    path = "/Users/hasansyed/Documents/CS-361-Microservice-for-partner/images/" + file_name
+    image = Image(filename=path)
+    data = image.get
+    response = data
 
     ch.basic_publish(exchange='',
                      routing_key=props.reply_to,
                      properties=pika.BasicProperties(correlation_id = \
                                                          props.correlation_id),
-                     body=str(response))
+                     body=response)
     ch.basic_ack(delivery_tag=method.delivery_tag)
 
 channel.basic_qos(prefetch_count=1)
